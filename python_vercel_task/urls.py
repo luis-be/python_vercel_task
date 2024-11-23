a94016ec2d5d6283b9d61979fdcf01d8d25a0fe9
Casiano Benitez <40897562+luis-be@users.noreply.github.com>
2024-11-23 10:10:29 -0300

    Corregido errores en vercel
    no puedo creer creo que la tarea esta terminada

--- python_vercel_task/urls.py
+++ python_vercel_task/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+     path('', lambda request: redirect('blog/', permanent=False)),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
 ]
